420cdbea8d98537eb6a7028edb73a503d274bb48
krasiren <voyosamo10@gmail.com>
2021-05-08 15:20:31 +0200

    Test basic komand v terminalu

--- Blockchain/src/Main.java
+++ Blockchain/src/Main.java
@@ -1,5 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("radi");
+        Process p;
+        StringBuffer output = new StringBuffer();
+        try {
+            p = Runtime.getRuntime().exec("ls");
+
+            p.waitFor();
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+            String line = "";
+            while ((line = reader.readLine())!= null) {
+                output.append(line + "\n");
+            }
+        } catch (IOException e) {
+
+        } catch (InterruptedException c) {
+
+        }
+        System.out.println(output);
     }
 }
